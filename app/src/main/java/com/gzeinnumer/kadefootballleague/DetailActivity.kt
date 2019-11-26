7f85b072b69d2f294ed8e470748007b836893155
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-11-26 09:45:25 +0700

    belum siap

--- app/src/main/java/com/gzeinnumer/kadefootballleague/DetailActivity.kt
+++ app/src/main/java/com/gzeinnumer/kadefootballleague/DetailActivity.kt
@@ -2,19 +2,51 @@ package com.gzeinnumer.kadefootballleague
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_data.view.*
+import org.jetbrains.anko.*
 
 class DetailActivity : AppCompatActivity() {
-
-    companion object{
+    companion object {
         val DATA = "data"
+        var item: Item? = null
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        DetailActivityUI().setContentView(this)
 
-        val item = intent.getParcelableExtra<Item>(DATA)
+
+        item = intent.getParcelableExtra<Item>(DATA)
+
+//        toast(item.title)
 
 
     }
+
+    class DetailActivityUI : AnkoComponent<DetailActivity> {
+        override fun createView(ui: AnkoContext<DetailActivity>) = with(ui) {
+            verticalLayout {
+                item?.icon?.let {
+                    Picasso.get().load(it).fit().into(img_item)
+                }
+                imageView {
+                    id = R.id.img_item
+                }.lparams {
+                    height = dip(75)
+                    width = dip(75)
+                }
+                textView {
+                    id = R.id.name_item
+                    textSize = 16f
+                    textAlignment = View.TEXT_ALIGNMENT_CENTER
+                }.lparams {
+                    margin = dip(16)
+                }
+            }
+        }
+    }
 }
+
--- app/src/main/java/com/gzeinnumer/kadefootballleague/FootBallAdapter.kt
+++ app/src/main/java/com/gzeinnumer/kadefootballleague/FootBallAdapter.kt
@@ -1,16 +1,19 @@
 package com.gzeinnumer.kadefootballleague
 
 import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_data.*
 import kotlinx.android.synthetic.main.item_data.view.*
-
-
+import org.jetbrains.anko.*
+import org.jetbrains.anko.cardview.v7.cardView
 
 class FootBallAdapter(private val context: Context,
                       private val list : MutableList<Item>,
@@ -18,7 +21,8 @@ class FootBallAdapter(private val context: Context,
     : RecyclerView.Adapter<FootBallAdapter.MyHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder
-            = MyHolder(LayoutInflater.from(context).inflate(R.layout.item_data, parent, false))
+            = MyHolder(FootballUI().createView(AnkoContext.create(parent.context, parent)))
+    //HokageViewHolder(HokageUI().createView(AnkoContext.create(parent.context, parent)))
 
     override fun getItemCount(): Int = list.size
 
@@ -34,6 +38,48 @@ class FootBallAdapter(private val context: Context,
             }
             containerView.setOnClickListener{
                 listener(item)
+            }
+        }
+    }
+
+    class FootballUI : AnkoComponent<ViewGroup> {
+        override fun createView(ui: AnkoContext<ViewGroup>): View {
+            return with(ui){
+                verticalLayout {
+                    lparams(width = matchParent, height = wrapContent)
+                    padding = dip(16)
+                    orientation = LinearLayout.HORIZONTAL
+
+                    cardView{
+                        padding = dip(10)
+                        cardElevation = dip(6).toFloat()
+                        background = GradientDrawable().apply {
+                            cornerRadius = 8f
+                        }
+                        verticalLayout {
+                            lparams(width = matchParent, height = wrapContent)
+                            padding = dip(16)
+                            orientation = LinearLayout.VERTICAL
+                            gravity = Gravity.CENTER
+
+                            imageView {
+                                id = R.id.img_item
+                            }.lparams {
+                                height = dip(75)
+                                width = dip(75)
+                            }
+                            textView {
+                                id = R.id.name_item
+                                textSize = 16f
+                                textAlignment = View.TEXT_ALIGNMENT_CENTER
+                            }.lparams {
+                                margin = dip(16)
+                            }
+                        }
+                    }.lparams {
+                        margin = dip(6)
+                    }
+                }
             }
         }
     }
